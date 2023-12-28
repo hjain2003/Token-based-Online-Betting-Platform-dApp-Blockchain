bdb70dcdcf6d4d69cdfb43ec19e30510fe9085ae
Harsh <hjain_be21@thapar.edu>
2023-12-28 09:16:06 +0530

    added frontend

--- smart_contract/truffle-config.js
+++ smart_contract/truffle-config.js
@@ -1,10 +1,8 @@
-require('dotenv').config({path:'/config.env'});
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-console.log('PRIVATE_KEY:', process.env.PRIVATE_KEY);
+const privateKeys = ['3061357d8958844656c9c148365d59c548500a7412021ce3a7b02c184ec7aaff'];
 
 module.exports = {
   networks: {
-
     sepolia: {
       provider: () => new HDWalletProvider({
         privateKeys: ['1d4e4a1694a233da4353987450d2ab19a0446a9cb966c9ab98a1a11b55168b6d'], // Replace with your private key
@@ -12,7 +10,6 @@ module.exports = {
       }),
       network_id: 11155111, // Sepolia network ID
       gasPrice: 1000000000, // Customize gas price if needed
-      gas: 8000000,
       confirmations: 2, // Number of confirmations to wait before deployment
       timeoutBlocks: 200, // Number of blocks before a deployment times out
       skipDryRun: true, // Skip dry run before migrations
@@ -21,7 +18,7 @@ module.exports = {
     sepoliaTest: {
       provider: () => new HDWalletProvider({
         privateKeys: ['1d4e4a1694a233da4353987450d2ab19a0446a9cb966c9ab98a1a11b55168b6d'], // Replace with your private key
-        providerOrUrl: 'https://eth-sepolia.g.alchemy.com/v2/UXtZgzfx45R50-CAX52-9lif04wIMnRW', // Sepolia testnet RPC URL
+        providerOrUrl: 'https://ethereum-sepolia.blockpi.network/v1/rpc/public', // Sepolia testnet RPC URL
       }),
       network_id:  11155111, // Sepolia testnet network ID
       gasPrice: 1000000000, // Customize gas price if needed
@@ -31,9 +28,10 @@ module.exports = {
       skipDryRun: true, // Skip dry run before migrations
       networkCheckTimeout: 1000000
     },
+
     polygon: {
       provider: () => new HDWalletProvider({
-        privateKeys: ['3061357d8958844656c9c148365d59c548500a7412021ce3a7b02c184ec7aaff'], // Replace with your private key
+        privateKeys: ['1d4e4a1694a233da4353987450d2ab19a0446a9cb966c9ab98a1a11b55168b6d'], // Replace with your private key
         providerOrUrl: 'https://rpc-mumbai.maticvigil.com',
       }),
       network_id: 80001, // Polygon network ID
@@ -44,6 +42,16 @@ module.exports = {
       skipDryRun: true, // Skip dry run before migrations
       networkCheckTimeout: 1000000
     },
+
+      
+      matic: {
+      provider: () => new HDWalletProvider(privateKeys,`wss://polygon-mumbai.infura.io/ws/v3/af2e33b48f9e469ca131d90ba0fdde05`,0,1),
+      network_id: 80001,
+      confirmations: 2,
+      networkCheckTimeout: 10000,
+      timeoutBlocks: 200,
+      skipDryRun:true,
+      }
   },
 
   mocha: {
