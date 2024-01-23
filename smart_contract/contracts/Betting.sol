d2710dee057fec4cbde31ef34a35eb6c78ecf89b
Harsh <hjain_be21@thapar.edu>
2024-01-23 10:39:36 +0530

    modified code

--- smart_contract/contracts/Betting.sol
+++ smart_contract/contracts/Betting.sol
@@ -4,25 +4,20 @@ pragma solidity ^0.8.11;
 contract Betting {
     address payable[] public players;
     uint public minBet;
-    uint public maxNoOfBets;
     address public manager;
     uint public randomResult;
-    uint public tokenPrice = 0.01 ether;
+    uint public tokenPrice = 0.1 ether;
     mapping(address => uint) public tokens;
 
     struct BetInfo {
-        address player;
-        uint totalBetAmount;
-    }
-
-    struct pplBetInfo {
         address better;
-        uint BetAmt;
-        uint betIndex;
+        address player;
+        uint BetAmount;
+        uint guessPosition;
     }
 
-    pplBetInfo[] public pplBets;
-    BetInfo[] public totalBets;
+  
+    BetInfo[] public totalBets; 
 
     constructor() {
         manager = msg.sender;
@@ -46,83 +41,80 @@ contract Betting {
         minBet = betTokens;
     }
 
-    function setMaxBets(uint numberParticipate) public onlyManager {
-        maxNoOfBets = numberParticipate;
-    }
-
     function enterComp() public {
         players.push(payable(msg.sender));
-        totalBets.push(BetInfo(msg.sender, 0));
     }
 
     function getPlayers() public view returns(address payable[] memory) {
         return players;
     }
 
-    function getPlayerInfo() public view returns (address[] memory, uint[] memory) {
-        address[] memory playerAddresses = new address[](players.length);
-        uint[] memory playerIndexes = new uint[](players.length);
+    function bet(uint _playerIndex, uint _guessPos, uint _amount) payable public {
+        require(_playerIndex < players.length, "Invalid player index");
+        require(tokens[msg.sender] >= minBet, "Insufficient bet amount");
 
-        for (uint i = 0; i < players.length; i++) {
-            playerAddresses[i] = players[i];
-            playerIndexes[i] = i;
-        }
+        BetInfo memory newBet = BetInfo({
+            better: msg.sender,
+            player: players[_playerIndex],
+            BetAmount: _amount,
+            guessPosition: _guessPos
+        });
 
-        return (playerAddresses, playerIndexes);
-    }
-
-    function bet(uint playerIndex, uint amount) payable public {
-        require(tokens[msg.sender] >= minBet, "insufficient bet amount");
-
-        totalBets[playerIndex].totalBetAmount += amount;
-        pplBets.push(pplBetInfo(msg.sender, amount, playerIndex));
-
-        tokens[msg.sender] -= amount;
-    }
+        totalBets.push(newBet);
+}
 
     function getRandomNumber() public view returns(uint) {
         return uint(keccak256(abi.encodePacked(manager, block.timestamp)));
     }
 
     function pickWinner() public onlyManager {
-        require(players.length > 0, "No players in the competition");
+    require(players.length > 0, "No players in the competition");
 
-        randomResult = getRandomNumber();
-        uint winnerIndex = randomResult % players.length; //winner index
+    randomResult = getRandomNumber();
+    uint winnerIndex = randomResult % players.length; //winner index
 
-        uint total_moneyEarned = 0;
+    uint totalTokens = 0;
+    uint managerCut;
 
-        for (uint i = 0; i < players.length; i++) {
-            total_moneyEarned += totalBets[i].totalBetAmount;
-        }
-
-        uint house_commission = (10 * total_moneyEarned) / 100;
-        tokens[manager] += house_commission;
-        uint winnings = total_moneyEarned - house_commission;
-
-        uint count = 0;
-        for (uint i = 0; i < pplBets.length; i++) {
-            if (pplBets[i].betIndex == winnerIndex) {
-                count++;
-            }
-        }
-
-        uint indv_winnings = (winnings / count);
-
-        for (uint i = 0; i < pplBets.length; i++) {
-            if (pplBets[i].betIndex == winnerIndex) {
-                tokens[pplBets[i].better] += indv_winnings;
-            }
-        }
-
-        //reset
-        for (uint i = 0; i < totalBets.length; i++) {
-            totalBets[i].totalBetAmount = 0;
-        }
-        delete pplBets;
-        delete players;
-        count = 0;
+    // Calculate the total amount of tokens in the contract
+    for (uint i = 0; i < totalBets.length; i++) {
+        totalTokens += totalBets[i].BetAmount;
     }
+
+    // Calculate manager's cut (10% of totalTokens)
+    managerCut = (totalTokens * 10) / 100;
+    tokens[manager] += managerCut;
+
+    // Calculate the amount to be distributed among winners (90% of totalTokens)
+    uint winnersCut = totalTokens - managerCut;
+
+    //loop to find out the total money bet by the winners combined
+    uint total_bet_of_winners;
+    for(uint i=0;i<totalBets.length;i++){
+        if(totalBets[i].guessPosition == winnerIndex){
+            total_bet_of_winners+=totalBets[i].BetAmount;
+        }
+    }
+
+    // Loop through totalBets to find winners and distribute tokens
+    for (uint i = 0; i < totalBets.length; i++) {
+        if (totalBets[i].guessPosition == winnerIndex) {
+            // This player is a winner
+            uint playerShare = (totalBets[i].BetAmount * winnersCut) / total_bet_of_winners;
+            tokens[totalBets[i].better] += playerShare;
+        }
+    }
+
+    resetGame();
+}
+
+function resetGame() internal {
+    // Reset game-related variables
+    delete players;
+    delete totalBets;
+    randomResult = 0;
+}
+
 
     function withdrawTokens(uint withdraw_token) public {
         require(withdraw_token <= tokens[msg.sender], "Not enough tokens");
